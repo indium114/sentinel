8bf81d42de6f056d4de648694ba46ace92d5fc44
indium114 <156162907+indium114@users.noreply.github.com>
2026-06-23 13:57:42 +0200

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# sentinel
+
+*sentinel* is a simple, extensible, and [gizmodic](https://gizmodic.software) directory watcher daemon.
+
+it is configured in **lua**, allowing you to specify what directories you want to watch, and what you want to happen whenever a certain event takes place.
---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "rust devshell and package, created by scaffolder";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "rust-devshell";
+
+          packages = with pkgs; [
+            cargo
+            rustc
+            rustfmt
+            rust-analyzer
+            clippy
+            pkg-config
+          ];
+        };
+
+        packages.sentinel = pkgs.rustPlatform.buildRustPackage {
+          name = "sentinel";
+          version = "0.1.0";
+
+          src = ./.;
+
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+
+        apps.sentinel = {
+          type = "app";
+          program = "${self.packages.${pkgs.stdenv.hostPlatform.system}.sentinel}/bin/sentinel";
+        };
+      });
+}
