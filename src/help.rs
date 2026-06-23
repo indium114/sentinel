ea05e263cc8c3c36b7a9a5f336a566e3e10d9eb7
indium114 <156162907+indium114@users.noreply.github.com>
2026-06-23 14:44:40 +0200

    feat: delegate watcher to its own function

--- src/help.rs
+++ src/help.rs
@@ -1,4 +1,5 @@
-use notify::EventKind;
+use notify::{Event, EventKind, RecursiveMode, Result, Watcher};
+use std::{path::Path, sync::mpsc};
 
 pub fn kind_of_event(event: &EventKind) -> String {
     if event.is_access() {
@@ -14,4 +15,28 @@ pub fn kind_of_event(event: &EventKind) -> String {
     } else {
         "unknown".to_string()
     }
+}
+
+pub fn spawn_watcher(path: &str, recursive: bool) -> Result<()> {
+    let (tx, rx) = mpsc::channel::<Result<Event>>();
+    let mut watcher = notify::recommended_watcher(tx)?;
+
+    let recurse = match recursive {
+        true => RecursiveMode::Recursive,
+        false => RecursiveMode::NonRecursive,
+    };
+
+    watcher.watch(Path::new(path), recurse)?;
+    for res in rx {
+        match res {
+            Ok(event) => {
+                println!("== NEW EVENT ==");
+                println!("event: {}", kind_of_event(&event.kind));
+                println!("paths: {:#?}", event.paths);
+            }
+            Err(e) => println!("error: {:#?}", e),
+        }
+    }
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,10 @@
-use notify::{Event, RecursiveMode, Result, Watcher};
+use notify::Result;
 use std::{path::Path, sync::mpsc};
 
 mod help;
 
 fn main() -> Result<()> {
-    let (tx, rx) = mpsc::channel::<Result<Event>>();
-    let mut watcher = notify::recommended_watcher(tx)?;
+    let path = "/home/indium114/temp";
 
-    watcher.watch(Path::new("/home/indium114/temp"), RecursiveMode::Recursive)?;
-    for res in rx {
-        match res {
-            Ok(event) => {
-                println!("== NEW EVENT ==");
-                println!("event: {}", help::kind_of_event(&event.kind));
-                println!("paths: {:#?}", event.paths);
-            }
-            Err(e) => println!("error: {:#?}", e),
-        }
-    }
-
-    Ok(())
+    help::spawn_watcher(path, true)
 }
