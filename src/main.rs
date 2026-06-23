933f7205cf385c63941459820647ab085d557078
indium114 <156162907+indium114@users.noreply.github.com>
2026-06-23 14:21:06 +0200

    feat: basic watching

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use notify::{Event, RecursiveMode, Result, Watcher};
+use std::{path::Path, sync::mpsc};
+
+fn main() -> Result<()> {
+    let (tx, rx) = mpsc::channel::<Result<Event>>();
+    let mut watcher = notify::recommended_watcher(tx)?;
+
+    watcher.watch(Path::new("/home/indium114/temp"), RecursiveMode::Recursive)?;
+    for res in rx {
+        match res {
+            Ok(event) => {
+                println!("== NEW EVENT ==");
+                println!("event: {:#?}", event);
+            },
+            Err(e) => println!("error: {:#?}", e),
+        }
+    }
+
+    Ok(())
 }
